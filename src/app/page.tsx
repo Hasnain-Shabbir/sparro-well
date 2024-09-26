655610180ac5d5aaf625003c995b7c2e2e583456
Hasnain-Shabbir <hasnainshabbir186@gmail.com>
2024-09-26 11:37:25 +0500

    feat(design-system: create button component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,95 @@
+'use client';
+import { LeftArrow } from '@/assets';
+import { Button } from '@/components/ui/button';
+
 export default function Home() {
   return (
-    <div className="">
-      <h1 className="text-6xl font-bold">Hello this is</h1>
-      <div className="size-32 bg-white shadow-xl"></div>
+    <div className="space-y-6 rounded-xl p-5">
+      <div className="space-x-5">
+        <Button variant="accent" size="sm">
+          Text here
+        </Button>
+        <Button variant="accent" size="md">
+          Text here
+        </Button>
+        <Button variant="accent" size="lg">
+          Text here
+        </Button>
+        <Button variant="accent" size="xl">
+          Text here
+        </Button>
+      </div>
+      <div className="space-x-5">
+        <Button variant="danger" size="sm">
+          Text here
+        </Button>
+        <Button variant="danger" size="md">
+          Text here
+        </Button>
+        <Button variant="danger" size="lg">
+          Text here
+        </Button>
+        <Button variant="danger" size="xl">
+          Text here
+        </Button>
+      </div>
+      <div className="space-x-5">
+        <Button variant="primary" size="sm">
+          Text here
+        </Button>
+        <Button variant="primary" size="md">
+          Text here
+        </Button>
+        <Button variant="primary" size="lg">
+          Text here
+        </Button>
+        <Button variant="primary" size="xl">
+          Text here
+        </Button>
+      </div>
+      <div className="space-x-5">
+        <Button variant="secondary" size="sm">
+          Text here
+        </Button>
+        <Button variant="secondary" size="md">
+          Text here
+        </Button>
+        <Button variant="secondary" size="lg">
+          Text here
+        </Button>
+        <Button variant="secondary" size="xl">
+          Text here
+        </Button>
+      </div>
+      <div className="space-x-5">
+        <Button variant="link" size="sm" href="https://www.google.com">
+          Text here
+        </Button>
+        <Button variant="link" size="md">
+          Text here
+        </Button>
+        <Button variant="link" size="lg">
+          Text here
+        </Button>
+        <Button variant="link" size="xl">
+          Text here
+        </Button>
+      </div>
+
+      <div className="space-x-5">
+        <Button variant="icon" size="sm">
+          <LeftArrow />
+        </Button>
+        <Button variant="icon" size="md">
+          <LeftArrow />
+        </Button>
+        <Button variant="icon" size="lg">
+          <LeftArrow />
+        </Button>
+        <Button variant="icon" size="xl">
+          <LeftArrow />
+        </Button>
+      </div>
     </div>
   );
 }
---
+++ src/assets/icons/LeftArrow.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const LeftArrow = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="25"
+      height="24"
+      fill="none"
+      viewBox="0 0 25 24"
+    >
+      <path
+        stroke="#222325"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="1.5"
+        d="M15.75 19l-7-7 7-7"
+      ></path>
+    </svg>
+  );
+};
+
+export default LeftArrow;
