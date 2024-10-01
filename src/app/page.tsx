d8b8548f96084286fa445b27fb77a66857a50bb2
Hasnain-Shabbir <hasnainshabbir186@gmail.com>
2024-10-01 15:33:50 +0500

    feat(design-system): create Radio button component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
 'use client';
-import { AppCheckbox } from '@/components';
 import React from 'react';
+import { RadioGroup } from '@/components/ui/radio-group';
+import { Radio } from '@/components';
 
 export default function Home() {
   return (
     <>
       <div className="p-20">
-        <AppCheckbox label="Label" id="label" size='md'/>
+        <RadioGroup defaultValue="option-1">
+          <Radio id="option-1" size="sm" label="Default" value="option-1" />
+          <Radio id="option-2" size="sm" label="Compromise" value="option-2" />
+          <Radio id="option-3" size="sm" label="Sacrifice" value="option-3" />
+        </RadioGroup>
       </div>
     </>
   );
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as AppCheckbox } from './custom/AppCheckbox/AppCheckbox';
+export { default as Radio } from './custom/Radio/Radio';
 export { default as TextField } from './custom/TextField';
 export { default as Toggle } from './custom/Toggle/Toggle';
