16968cc1095fab5115f100b466d909ea3aeb6283
Hasnain-Shabbir <hasnainshabbir186@gmail.com>
2024-10-01 16:01:49 +0500

    feat(design-system): create Tag component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,12 @@
 'use client';
 import React from 'react';
-import { RadioGroup } from '@/components/ui/radio-group';
-import { Radio } from '@/components';
+import { Tag } from '@/components';
 
 export default function Home() {
   return (
     <>
       <div className="p-20">
-        <RadioGroup defaultValue="option-1">
-          <Radio id="option-1" size="sm" label="Default" value="option-1" />
-          <Radio id="option-2" size="sm" label="Compromise" value="option-2" />
-          <Radio id="option-3" size="sm" label="Sacrifice" value="option-3" />
-        </RadioGroup>
+        <Tag label="Label" size="md" color='gray' />
       </div>
     </>
   );
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export { default as AppCheckbox } from './custom/AppCheckbox/AppCheckbox';
 export { default as Radio } from './custom/Radio/Radio';
+export { default as Tag } from './custom/Tag/Tag';
 export { default as TextField } from './custom/TextField';
 export { default as Toggle } from './custom/Toggle/Toggle';
