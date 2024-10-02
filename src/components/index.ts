f08bab71e5744f168b9e59fda05d9b094285def6
Hasnain-Shabbir <hasnainshabbir186@gmail.com>
2024-10-02 17:28:36 +0500

    feat(design-system): create design system page with all the components

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,4 @@ export { default as Tag } from './custom/Tag/Tag';
 export { default as TextField } from './custom/TextField';
 export { default as Toggle } from './custom/Toggle/Toggle';
 export { default as Typography } from './custom/Typography/Typography';
+export { default as AppBreadcrumbs } from './custom/AppBreadcrumbs/AppBreadcrumbs';
