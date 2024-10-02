9c3912e5986e299e3b45272398ccb42aeefb4b7d
Hasnain-Shabbir <hasnainshabbir186@gmail.com>
2024-10-02 10:45:35 +0500

    feat(design-system): create Pagination component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,43 @@
 'use client';
 import React from 'react';
 import { Tag } from '@/components';
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from '@/components/ui/pagination';
 
 export default function Home() {
   return (
     <>
       <div className="p-20">
-        <Tag label="Label" size="md" color='gray' />
+        <Tag label="Label" size="md" color="gray" />
+        <Pagination>
+          <PaginationContent>
+            <PaginationItem>
+              <PaginationPrevious href="#" />
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink href="#">1</PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink href="#">2</PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink href="#">3</PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationEllipsis />
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationNext href="#" />
+            </PaginationItem>
+          </PaginationContent>
+        </Pagination>
       </div>
     </>
   );
